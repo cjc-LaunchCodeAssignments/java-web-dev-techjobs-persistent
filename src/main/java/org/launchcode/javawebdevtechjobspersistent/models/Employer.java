4ec0d40496e144009fdf47c811f3723c87d5fc53
cjconnor18 <cjallen1988@gmail.com>
2020-03-04 19:26:21 -0600

    part 2 data layer...created EmployerRepository and SkillRepository extending CrudRepository

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,29 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(max = 50)
+    private String location;
 
+    Employer () {
+    }
+
+    Employer(String location) {
+        super();
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.Size;
+
 public class Skill extends AbstractEntity {
 
+
+    private String longDescription;
+
+    Skill () {
+    }
+
+    Skill (String longDescription) {
+        super();
+        this.longDescription = longDescription;
+    }
+
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
 }
